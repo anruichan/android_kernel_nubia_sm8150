837fbb41a3258a878854b3788761e6f816615bee
anruichan <anrui2032@gmail.com>
2025-01-24 14:07:16 +0800

    suspend: Import nubia changes
    Change-Id: I6fb3a4262f9bc45af9b47fa0a311353241e66c19

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -56,6 +56,11 @@ EXPORT_SYMBOL_GPL(pm_suspend_target_state);
 
 unsigned int pm_suspend_global_flags;
 EXPORT_SYMBOL_GPL(pm_suspend_global_flags);
+// Begin [0016004715 add the kernel power code, 20180316]
+#ifdef CONFIG_ZTEMT_POWER_DEBUG
+bool wakeup_wake_lock_debug = false;
+#endif
+// End [0016004715 add the kernel power code, 20180316]
 
 static const struct platform_suspend_ops *suspend_ops;
 static const struct platform_s2idle_ops *s2idle_ops;
@@ -497,6 +502,11 @@ int suspend_devices_and_enter(suspend_state_t state)
 	error = platform_suspend_begin(state);
 	if (error)
 		goto Close;
+// Begin [0016004715 add the kernel power code, 20180316]
+#ifdef CONFIG_ZTEMT_POWER_DEBUG
+	wakeup_wake_lock_debug = true;
+#endif //CONFIG_ZTEMT_POWER_DEBUG
+// End [0016004715 add the kernel power code, 20180316]
 
 	suspend_console();
 	suspend_test_start();
