d01612194eefd55c450461106a19f286b83fa1e1
anruichan <anrui2032@gmail.com>
2025-01-24 13:37:22 +0800

    ext4: Import nubia changes
    Change-Id: I41ef7ebc304190ad82d0813cf5324ec81f15d3f0

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3128,8 +3128,19 @@ static ext4_group_t ext4_has_uninit_itable(struct super_block *sb)
 		if (!gdp)
 			continue;
 
+#ifdef CONFIG_BOARD_NUBIA
+		if (gdp->bg_flags & cpu_to_le16(EXT4_BG_INODE_ZEROED))
+			continue;
+		if (group != 0)
+			break;
+		ext4_error(sb,"Inode table for bg 0 marked as "
+			   "needing zeroing");
+		if (sb_rdonly(sb))
+			return ngroups;
+#else
 		if (!(gdp->bg_flags & cpu_to_le16(EXT4_BG_INODE_ZEROED)))
 			break;
+#endif
 	}
 
 	return group;
